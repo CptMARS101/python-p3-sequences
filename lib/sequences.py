40eea4f737ec5e48c4bdda51b9364b7929a46e10
Marcus Kitchen <marcusdkitchen@gmail.com>
2024-01-16 14:10:40 -0500

    seq lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0,1])
+    else:
+        list = [0, 1]
+        while len(list) < length:
+            next_fib = list[-1] + list[-2]
+            list.append(next_fib)
+
+        print(list)
